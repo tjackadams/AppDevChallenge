4c7c0fd4300728029fbd594186829cac6dce5e3b
Thomas Adams <tjackadams@itadams.co.uk>
2019-04-15 11:45:14 +0100

    added a get latest alarms stored proc.
    #5

---
+++ SecurityMonitor/SecurityMonitor.Database/StoredProcedures/usp_GetAllLatestAlarms.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[usp_GetAllLatestAlarms]
+AS
+	;WITH SortedDeviceEvents AS
+	(
+		SELECT
+			[DeviceEvents].[Id],
+			[DeviceEvents].[DeviceId],
+			[DeviceEvents].[EventTime],
+			[DeviceEvents].[Status],
+			[Devices].[ImageUrl],
+			[Devices].[Latitude],
+			[Devices].[Longitude],
+			[Devices].[Name],
+			ROW_NUMBER() OVER (PARTITION BY [DeviceId] ORDER BY [EventTime] DESC) AS RowNumber
+		FROM
+			[DeviceEvents]	
+		INNER JOIN [Devices] ON [DeviceEvents].[DeviceId] = [Devices].[Id]
+	)
+	SELECT
+		[Id] as [AlarmId],
+		[DeviceId],
+		[EventTime],
+		[ImageUrl],
+		[Latitude],
+		[Longitude],
+		[Name],
+		[Status]
+	FROM
+		SortedDeviceEvents
+	WHERE
+		SortedDeviceEvents.RowNumber = 1
--- SecurityMonitor/src/SecurityMonitor.Core/Models/Alarm.cs
+++ SecurityMonitor/src/SecurityMonitor.Core/Models/Alarm.cs
@@ -6,13 +6,14 @@ namespace SecurityMonitor.Core.Models
 {
     public class Alarm
     {
+        public Guid AlarmId { get; set; }
         public int DeviceId { get; set; }
-        public string Name { get; set; }
+        public DateTimeOffset EventTime { get; set; }
+        public string ImageUrl { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public string Text { get; set; }
+        public string Name { get; set; }  
         public Status Status { get; set; }
-        public string ImageUrl { get; set; }
-
+        public string Text { get; set; }
     }
 }
